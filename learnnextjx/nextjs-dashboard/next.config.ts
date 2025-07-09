026bf3cb622d1a34d7ad8420d8d485f2b1b01c5b
saadmuk123 <amir.saad@aun.edu.ng>
2025-07-09 14:30:51 +0100

    ch 1-5 tut complete

--- learnnextjx/nextjs-dashboard/next.config.ts
+++ learnnextjx/nextjs-dashboard/next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
  
-  typescript: {
-    // This will make sure that TypeScript errors are treated as build errors
-    ignoreBuildErrors: false,
-  },
 };
 
 export default nextConfig;
